19f56157cbc2be5d949aab1d003adcb0f7fc8485
Zak <zak.afir@gmail.com>
2024-12-26 07:51:12 +0100

    use lazy row

--- app/src/main/java/com/zak/afir/composepl/basic_layout/LazyLayoutDemo.kt
+++ app/src/main/java/com/zak/afir/composepl/basic_layout/LazyLayoutDemo.kt
@@ -4,11 +4,14 @@ package com.zak.afir.composepl.basic_layout
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,6 +19,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.zak.afir.composepl.ui.theme.ComposePLTheme
+import kotlin.random.Random
 
 @Composable
 fun LazyListDemo(modifier: Modifier = Modifier) {
@@ -40,6 +44,16 @@ fun LazyListDemo(modifier: Modifier = Modifier) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .background(Color.Red)
+            )
+        }
+    }
+
+    LazyRow(modifier = Modifier.fillMaxSize(), contentPadding = PaddingValues(16.dp)) {
+        items(100) {
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(Color(Random.nextInt()))
             )
         }
     }
